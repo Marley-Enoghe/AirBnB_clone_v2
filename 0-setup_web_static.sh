b0d855e0cb4780dd785dd1fd87be1b7624f3cee2
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-12-10 04:14:45 -0800

    set up server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 # a Bash script that sets up a web servers for the deployment of web_static
 
-# install NGINX if not installed
-if ! command -v nginx &> /dev/null
-then
-        sudo apt update
-        sudo apt install nginx
-        sudo service nginx start
-else
-        echo "NGINX is already installed"
-fi
+sudo apt-get update
+sudo apt-get -y install nginx
 
 mkdir -p /data/web_static/{shared,releases/test/}
 echo "Welcome, Earthling!" > /data/web_static/releases/test/index.html
